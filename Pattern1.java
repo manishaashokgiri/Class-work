c73eb070007414e9ed7cf776988af1a488e58ce8
Manisha Giri <115716862+manishaashokgiri@users.noreply.github.com>
2024-03-06 12:17:10 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,19 @@
+package com.patternexample;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i,j;
+        
+        for(i=1; i<=5; i++)
+        {
+        	for(j=1; j<=i; j++)
+        	{
+        		System.out.print("*");
+        	}
+        	System.out.println();
+        }
+	}
+
+}
---
+++ Pattern4.java
@@ -0,0 +1,19 @@
+package com.patternexample;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1; i<=5; i++)
+		{
+			for(j=5; j>=i; j--)
+			{
+			 System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
